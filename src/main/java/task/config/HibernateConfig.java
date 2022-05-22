8de022c53a646c1d8e024717814cfe06fb9a97ef
zmo00 <zmo00@yandex.ru>
2022-05-22 16:10:04 +0300

    the end

--- src/main/java/task/config/HibernateConfig.java
+++ src/main/java/task/config/HibernateConfig.java
@@ -1,17 +1,25 @@
 package task.config;
 
 import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import task.models.User;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 import java.util.Properties;
 
@@ -38,24 +46,62 @@ public class HibernateConfig {
         return dataSource;
     }
 
+    //Реализация через SessionFactory
+//    @Bean
+//    public LocalSessionFactoryBean getSessionFactory() {
+//        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+//        factoryBean.setDataSource(getDataSource());
+//
+//        Properties props=new Properties();
+//        props.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+//        props.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+//
+//        factoryBean.setHibernateProperties(props);
+//        factoryBean.setAnnotatedClasses(User.class);
+//        return factoryBean;
+//    }
+//
+//    @Bean
+//    public HibernateTransactionManager getTransactionManager() {
+//        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+//        transactionManager.setSessionFactory(getSessionFactory().getObject());
+//        return transactionManager;
+//    }
+
+
+    //Реализация через EntityManager
     @Bean
-    public LocalSessionFactoryBean getSessionFactory() {
-        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
-        factoryBean.setDataSource(getDataSource());
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean em
+                = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(getDataSource());
+        em.setPackagesToScan("task");
 
-        Properties props=new Properties();
-        props.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
-        props.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        em.setJpaProperties(additionalProperties());
 
-        factoryBean.setHibernateProperties(props);
-        factoryBean.setAnnotatedClasses(User.class);
-        return factoryBean;
+        return em;
     }
 
     @Bean
-    public HibernateTransactionManager getTransactionManager() {
-        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-        transactionManager.setSessionFactory(getSessionFactory().getObject());
+    public PlatformTransactionManager transactionManager() {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+
         return transactionManager;
+    }
+
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor exceptionTranslation(){
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+
+    Properties additionalProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+        properties.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+
+        return properties;
     }
 }
--- src/main/java/task/dao/UserDaoImpl.java
+++ src/main/java/task/dao/UserDaoImpl.java
@@ -5,43 +5,95 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import task.models.User;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class UserDaoImpl implements UserDao{
 
-    private SessionFactory sessionFactory;
+    //Реализация через SessionFactory
+//    private SessionFactory sessionFactory;
+//
+//    @Autowired
+//    public UserDaoImpl(SessionFactory sessionFactory) {
+//        this.sessionFactory = sessionFactory;
+//    }
+//
+//    @Override
+//    public void create(User user) {
+//        sessionFactory.getCurrentSession().save(user);
+//    }
+//
+//    @Override
+//    public User read(long id) {
+//        return (User) sessionFactory.getCurrentSession().get(User.class, id);
+//    }
+//
+//    @Override
+//    public void update(User user) {
+//        sessionFactory.getCurrentSession().update(user);
+//    }
+//
+//    @Override
+//    public void delete(User user) {
+//        sessionFactory.getCurrentSession().delete(user);
+//    }
+//
+//    @Override
+//    @SuppressWarnings("unchecked")
+//    public List<User> getUsers() {
+//        TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+//        return query.getResultList();
+//    }
 
-    @Autowired
-    public UserDaoImpl(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
+
+    //Реализация через EntityManager
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Override
     public void create(User user) {
-        sessionFactory.getCurrentSession().save(user);
+        entityManager.persist(user);
+        entityManager.flush();
     }
 
     @Override
     public User read(long id) {
-        return (User) sessionFactory.getCurrentSession().get(User.class, id);
+        return entityManager.find(User.class, id);
     }
 
     @Override
     public void update(User user) {
-        sessionFactory.getCurrentSession().update(user);
+        User updateUser = entityManager.find(User.class, user.getId());
+        entityManager.detach(updateUser);
+        updateUser.setName(user.getName());
+        updateUser.setLastname(user.getLastname());
+        updateUser.setAge(user.getAge());
+        updateUser.setEmail(user.getEmail());
+        entityManager.merge(updateUser);
+        entityManager.flush();
     }
 
     @Override
     public void delete(User user) {
-        sessionFactory.getCurrentSession().delete(user);
+        entityManager.remove(entityManager.find(User.class, user.getId()));
+        entityManager.flush();
     }
 
     @Override
-    @SuppressWarnings("unchecked")
     public List<User> getUsers() {
-        TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-        return query.getResultList();
+        List<Object[]> objects =
+                entityManager.createQuery("select id, name, lastname, age, email from User").getResultList();
+        List<User> userList = new ArrayList<User>();
+        for (Object[] object : objects) {
+            User user = new User((String) object[1], (String) object[2], (Byte) object[3], (String) object[4]);
+            user.setId((Long) object[0]);
+            userList.add(user);
+        }
+        return userList;
     }
 }
